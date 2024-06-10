a059335f94a49a6b35205fe87a471ca0e5ca7974
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-06-09 21:04:55 -0500

    Added event listeners and error handling.

--- bot.py
+++ bot.py
@@ -1,16 +1,66 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+    print(
+          f'{client.user.name} has connected to Discord!\n'
+          f'{guild.name}(id: {guild.id})'
+          )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+    if "Happy Birthday" in message.content and message.mentions:
+        response = '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉'
+        await message.channel.send(response)
+    if message.content == 'raise-exception':
+        raise discord.DiscordException
+    
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
 client.run(TOKEN)
